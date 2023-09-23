579d070e119b0ca94870b7aec0ee8b89bfc35ec6
xavierlw2021 <84850722+xavierlw2021@users.noreply.github.com>
2023-09-23 10:01:28 +0800

    Add files via upload

---
+++ hailAndChartered/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.generic import View, TemplateView
+import base64, pickle
+
+from hailAndChartered import models
+import datetime
+
+
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import *
+
+from urllib.parse import quote, parse_qsl
+import uuid
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+# def confirmed(request):
+#     transaction_id = request.GET['transactionId']
+#     order = models.car_order.objects.filter(transaction_id = transaction_id).first()
+#     if order:
+#         line_pay = payService.LinePay()
+#         line_pay.confirm(transaction_id=transaction_id, amount=order.total_cost)
+                
+#         order.is_pay = True #確認收款無誤時,改成已付款
+#         order.save()
+        
+#         #傳收據給用戶
+#         message = payService.receipt(order) 
+#         line_bot_api.push_message(to=order.quest_phone.line_userId, messages=message)
+#         thxMsg = '付款已完成，感謝您的消費'
+#         return render(request, 'paySuccess.html', {'thxMsg': thxMsg}) #((要改))render之類的
+
+@csrf_exempt
+def callback(request):
+    if request.method=='POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+        message = []
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                uid = event.source.user_id
+                profile = line_bot_api.get_profile(uid)
+                name = profile.display_name                
+                msgtext = event.message.text
+                
+                #訊息判斷區
+                if '@關於我們' in msgtext:
+                    message.append(TextSendMessage(text="本聊天室為包車叫車測試"))
---
+++ qrCardAndCar/settings.py
@@ -0,0 +1,114 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+LINE_CHANNEL_ACCESS_TOKEN = 'xHnaVuyXvKzJ0yi02gSPpjVJBEnWfMCRrmUsWpUhh16VnDYqkgaXKg0HwC1Ul/m4MyWdZTSW66OZb9L6Hw0rHd/StJ7Olu1ShFs5R3U/GIxQ7zXdrcjMIYQUPxYJ1ws4G2VX++lOFHU6msuKdZOqPwdB04t89/1O/w1cDnyilFU='
+
+LINE_CHANNEL_SECRET = '7ab67c9f982fb7fd0011e63c702a71c2'
+
+SECRET_KEY = "django-insecure-9k9(y_$jl+x$h3a4jox3-28d15fq93dxmr=6)929yiktxsfoa%"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    'import_export',
+    'hailAndChartered.apps.HailandcharteredConfig',
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "qrCardAndCar.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "qrCardAndCar.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cardTest',
+        'USER': 'postgres',
+        'PASSWORD':'5r5o8t0e7r0',
+        'HOST':'localhost',
+        'PORT':'5432',
+        'CONN_MAX_AGE':500,
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = 'zh-Hant'
+TIME_ZONE = 'Asia/Taipei'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = "static/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+IMPORT_EXPORT_USE_TRANSACTIONS = True
