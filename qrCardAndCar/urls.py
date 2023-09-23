cc0c8b22be27ab5aacc660126381252545356710
xavierlw2021 <84850722+xavierlw2021@users.noreply.github.com>
2023-09-23 10:53:43 +0800

    Add files via upload

--- qrCardAndCar/urls.py
+++ qrCardAndCar/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+import hailAndChartered.views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),    
+    re_path(r'^callback', hailAndChartered.views.callback),
 ]
