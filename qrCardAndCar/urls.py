3b1e03e3d8722c746b4b381d6902c33be4ac724f
xavierlw2021 <84850722+xavierlw2021@users.noreply.github.com>
2023-09-27 22:51:40 +0800

    Delete qrCardAndCar directory

--- qrCardAndCar/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, re_path
-import hailAndChartered.views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),    
-    re_path(r'^callback', hailAndChartered.views.callback),
-]
